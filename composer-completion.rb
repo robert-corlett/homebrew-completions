05907372865d2952e6873a4b62f366de94af4e66
Tony Li <tonyli.lives@gmail.com>
2015-11-04 21:25:04 -0800

    add description and use sha256

--- composer-completion.rb
+++ composer-completion.rb
@@ -1,7 +1,8 @@
 class ComposerCompletion < Formula
+  desc "Bash completion for Composer"
   homepage "https://gist.github.com/tonglil/753d59d6d649f85600fe"
   url "https://gist.githubusercontent.com/tonglil/753d59d6d649f85600fe/raw/854fcb2f5a4cae824119d6c5c40a2bde4e541689/composer-completion.sh"
-  sha1 "21bedfccf0a9cdf23b7b1a7fd5fee4188a72fadd"
+  sha256 "6a9e49ed748e5f7a7fbfb6ecbea2c54a5ba3832444d58f81208bb2768754e2da"
 
   def install
     bash_completion.install "composer-completion.sh"
