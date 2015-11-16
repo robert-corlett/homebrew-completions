fdfe50dd2b09b07076c3ba0f288c9eaf69f6ae8f
Robert Corlett <xcorlett@gmail.com>
2015-11-15 16:00:56 -1000

    Update composer-completion.rb

--- composer-completion.rb
+++ composer-completion.rb
@@ -1,9 +1,8 @@
 class ComposerCompletion < Formula
   desc "Bash completion for Composer"
-  homepage "https://gist.github.com/tonglil/753d59d6d649f85600fe"
-  url "https://gist.githubusercontent.com/tonglil/753d59d6d649f85600fe/raw/854fcb2f5a4cae824119d6c5c40a2bde4e541689/composer-completion.sh"
-  version "1"
-  sha256 "6a9e49ed748e5f7a7fbfb6ecbea2c54a5ba3832444d58f81208bb2768754e2da"
+  homepage "https://gist.github.com/xcorlett/6292dbf542eb72656c8a"
+  url "https://gist.githubusercontent.com/xcorlett/6292dbf542eb72656c8a/raw/ab5a1e2a4a2d96ffe7efc327317d0f000a26f696/composer-completion.sh"
+  version "1.1"
 
   def install
     bash_completion.install "composer-completion.sh"
